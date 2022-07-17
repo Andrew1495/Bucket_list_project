f0f1e4cbda465185cbf91b5cb261cd162d7fc2e1
Andrew <andrewfairley95@gmail.com>
2022-07-17 15:59:49 +0100

    profile displays users bucket_list

--- application/controllers/user_controller.py
+++ application/controllers/user_controller.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, Blueprint, request
 import repositories.user_repo as user_repo
-
+import repositories.want_to_visit_repo as want_to_visit_repo
 
 
 user_blueprint = Blueprint("user", __name__)
@@ -14,6 +14,7 @@ def users():
 @user_blueprint.route("/users/<id>")
 def user(id):
     user = user_repo.select(id)
-    return render_template("users/user.html", user=user)
+    bucket_list = want_to_visit_repo.select_all()
+    return render_template("users/user.html", user=user, bucket_list=bucket_list)
 
 
